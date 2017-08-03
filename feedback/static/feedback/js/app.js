298eac4e46978b7f3d2b1674fb2bc915574122c4
Eric K <eric.nzai@asandk.com>
2017-08-03 20:28:56 +0300

    Add automatic scroll for customer comments

--- feedback/static/feedback/js/app.js
+++ feedback/static/feedback/js/app.js
@@ -1,7 +1,62 @@
-/**
- * Created by dan_the_great on 8/2/2017.
- */
-$(document).ready(function(){
-       $(".lorem").fadeIn(6000);
-       console.log('past fade')
-   })
+$(document).ready(function() {
+
+     $('.reviews').bxSlider({
+        speed: 500,
+        auto: true,
+        controls: true,
+        captions: false,
+        pager: true,
+        nextText: '',
+        prevText: '',
+        mode: 'vertical'
+
+
+    });
+
+    $('.reviews').bxSlider({
+        speed: 'fast',
+        auto: true,
+        controls: true,
+        captions: false,
+        pager: true,
+        nextText: '',
+        prevText: '',
+        mode: 'vertical',
+        onSliderLoad: function () {
+
+            $(".holder").css({ display: "block" });
+
+        }
+    });
+   
+
+});
+
+function animateContent(direction) {  
+    var animationOffset = $('.company').height() - $('.reviews').height()-30;
+    if (direction == 'up') {
+        animationOffset = 0;
+    }
+
+    console.log("animationOffset:"+animationOffset);
+    $('.reviews').animate({ "marginTop": (animationOffset)+ "px" }, 5000);
+}
+
+function up(){
+    animateContent("up")
+}
+function down(){
+    animateContent("down")
+}
+
+function start(){
+ setTimeout(function () {
+    down();
+}, 1000);
+ setTimeout(function () {
+    up();
+}, 1000);
+   setTimeout(function () {
+    console.log("wait...");
+}, 5000);
+}    
