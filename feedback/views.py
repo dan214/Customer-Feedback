85852cfec70bd0af5749335d0b82c270b8b99d36
Eric K <eric.nzai@asandk.com>
2017-08-20 13:19:02 +0300

    Add user groups home pages and update index view

--- feedback/views.py
+++ feedback/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.http import HttpResponseRedirect, Http404
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from .forms import CompanyForm
 
 
@@ -16,19 +16,50 @@ def detail(request, company_id):
     return render(request,'detail.html',{'company': company})
 
 
-def logout_view(request):
-    logout(request)
-
-
-@login_required(login_url='/accounts/login/')
 def index(request):
-    company_list = Company.objects.filter(employee= request.user)
+    is_employee = request.user.groups.filter(name='Employees').exists()
+    is_manager = request.user.groups.filter(name='Managers').exists()
 
-    context = {
-        "objects":company_list,
-    }
+    if is_employee:
+        company_list = Company.objects.filter(employee=request.user)
+        context = {
+            "objects": company_list,
+            "is_employee": is_employee,
+            "is_manager": is_manager
+        }
 
-    return render(request, 'index.html', context)
+        return render(request, 'employee_index.html', context)
+    elif request.user.is_staff:
+        company_list = Company.objects.all()
+        employees = User.objects.filter(groups__name='Employees')
+        managers = User.objects.filter(groups__name='Managers')
+
+        context = {
+            "objects": company_list,
+            "employees": employees,
+            "managers": managers
+        }
+
+        return render(request, 'admin_index.html', context)
+    elif is_manager:
+        employees = User.objects.filter(groups__name='Employees')
+        companies = Company.objects.all()
+
+        context = {
+            "employees": employees,
+            "companies": companies
+        }
+
+        return render(request,'manager_index.html', context)
+    else:
+        companies = Company.objects.all()
+
+        context = {
+            "companies": companies,
+        }
+
+        return render(request,'customer_index.html', context)
+
 
 def thanks(request):
     return render(request,'thank-you.html')
